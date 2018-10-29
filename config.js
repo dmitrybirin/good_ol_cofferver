ae9ae3de270c0753f670408090a809fa12776b93
Dmitry Birin <dmitry.birin@pipedrive.com>
2018-10-29 22:31:14 +0200

    origin to config

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const AUTH_CONFIG = {
 	clientId: 'sjCx8tDOXrkydooBnvyMCClbQO8t1MwS',
 	apiClientId: 'mB7nd3eKT9NdzvtDo8rdienpk69vAmt0',
 	domain: 'https://cofferver.eu.auth0.com',
+	allowedOrigin: 'https://coffeewheel.xyz',
 }
 
 module.exports = AUTH_CONFIG
